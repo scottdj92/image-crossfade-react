503586cc9ef53cb6fb759f047369ecc6f6f4a0bd
Scott Jones <scottdj92@gmail.com>
2018-08-25 08:54:16 -0400

    add-component: add development mode
    * update readme
    * add unit test file

--- dev/index.tsx
+++ dev/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import CrossFadeImage from "../index";
 
-const App = () => (
+const App: React.SFC = () => (
     <CrossFadeImage src="https://www.placecage.com/200/300"/>
 );
 
